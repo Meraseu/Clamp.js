552b2d6b5993cb7d9ae82bbb6182e726873a26c0
meraseudev <meraseudev@gmail.com>
2017-11-01 17:57:55 +0900

    example 추가작업 중

--- clamp.js
+++ clamp.js
@@ -1,9 +1,10 @@
 ;(function(Clamp) {
 
+    console.log(Clamp);
+
     Clamp = function(element, opts) {
-        
         this.element = element;
-        this.opts = opts;
+        this.opts = opts || {};
         this.text = this.element.textContent;
         this.scrollSize = 0;
         this.offsetSize = 0;
@@ -14,46 +15,34 @@
         this.lastWord = '';
         this.lastCharacter = '';
         this.regex = /\s+/gi;
-        this.added = '...';
-
+        this.added = this.opts.added || '...';
         this.init();
-
     }
     Clamp.prototype = {
         init : function() {
-
             this.backdrop = document.createElement('div');
-
             var text = document.createTextNode(this.text);
-
             this.backdrop.className = 'backdrop';
             this.backdrop.appendChild(text);
             this.element.appendChild(this.backdrop);
             this.backdrop.classList.add('nowrap');
-
             this.scrollSize = this.getScrollSize();
             this.offsetSize = this.getOffsetSize();
-
             if(this.scrollSize > this.offsetSize) {
                 this.setClamp();
                 this.backdrop.setAttribute('aria-hidden','true');
             } else {
                 this.element.removeChild(this.backdrop);
             }
-
         },
         setClamp : function() {
-
             this.word = this.text.trim().replace(this.regex, ' ').split(' ');
-
             while (this.getScrollSize() > this.getOffsetSize()) {
                 this.deleteArrayWord();
                 this.backdrop.textContent = this.setJoinWord() + ' ...';
             }
-
             this.backdrop.classList.remove('nowrap');
             this.setClampCharacater();
-
         },
         getWordCount : function() {
             return this.word.length;
@@ -83,24 +72,17 @@
             return this.backdrop.offsetHeight;
         },
         setClampCharacater : function() {
-
             this.backdrop.classList.add('nowrap');
             this.backdrop.textContent = this.setJoinWord() + ' ' + this.lastWord + this.added;
-
             this.character = this.lastWord.split('');
-
             this.deleteArrayCharacter();
             this.backdrop.textContent = this.setJoinWord() + ' ' + this.setJoinCharacter() +  this.added;
-
             while (this.getScrollSize() > this.getOffsetSize()) {
                 this.deleteArrayCharacter();
                 this.backdrop.textContent = this.setJoinWord() + ' ' + this.setJoinCharacter() + this.added;
             }
-
             this.backdrop.textContent = this.setJoinWord() + ' ' + this.setJoinCharacter() + this.lastCharacter + this.added;
-
             this.backdrop.classList.remove('nowrap');
-
         }
-
+    }
 })(window.Clamp = window.Clamp || {});
